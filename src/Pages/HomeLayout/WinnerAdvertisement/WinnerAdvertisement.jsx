f3cb3322f069e0ab9cd75e9d518314e9fac4635a
Habib0088 <sarkerbatch2k19@gmail.com>
2025-12-16 22:44:13 +0600

    Footer section done

--- src/Pages/HomeLayout/WinnerAdvertisement/WinnerAdvertisement.jsx
+++ src/Pages/HomeLayout/WinnerAdvertisement/WinnerAdvertisement.jsx
@@ -26,16 +26,36 @@ const WinnerAdvertisement = () => {
               src={winner?.[0]?.participants?.[0]?.participantPhoto}
               className="max-w-sm rounded-lg shadow-2xl flex-1"
             />
-            <h1 className="font-bold text-2xl">Name : {winner?.[0]?.participants?.[0]?.participantName}</h1>
-            <h1 className="font-bold text-2xl">Email : {winner?.[0]?.participants?.[0]?.participantEmail}</h1>
+            <h1 className="font-bold text-2xl">
+              Name : {winner?.[0]?.participants?.[0]?.participantName}
+            </h1>
+            <h1 className="font-bold text-2xl">
+              Email : {winner?.[0]?.participants?.[0]?.participantEmail}
+            </h1>
             <h2 className="font-bold ">Won : {winner?.[0]?.price} $</h2>
           </div>
 
           <div>
-            <h1 className="text-5xl font-bold flex-1 capitalize">{winner?.[0]?.name}-{winner?.[0]?.contestType}</h1>
+            <h1 className="text-5xl font-bold flex-1 capitalize">
+              {winner?.[0]?.name}-{winner?.[0]?.contestType}
+            </h1>
             <p className="py-6 font-bold">{winner?.[0]?.description}</p>
           </div>
         </div>
+      </div>
+      <div className="space-y-4">
+        <h1 className="text-center font-bold text-4xl">
+          Challenge Your Limits
+        </h1>
+        <p className=" mx-auto  font-bold text-center md:w-8/12 ">
+          Participation is the first step toward growth and success. It builds
+          confidence and encourages learning through experience. By
+          participating, we challenge our limits and discover our strengths.
+          Every effort, win or loss, becomes a valuable lesson. Participation
+          fosters teamwork, creativity, and resilience. It turns opportunities
+          into achievements and ideas into action. True inspiration begins when
+          we choose to take part
+        </p>
       </div>
     </div>
   );
--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
-
+import React from "react";
+import logo from "../../../assets/ChallengeHive.png";
+import { FaFacebook, FaLinkedin } from "react-icons/fa";
 const Footer = () => {
   return (
-    <div className='bg-red-500'>
-      <h1>Footer</h1>
+    <div className="w-11/12 mx-auto py-5  text-center md:text-start">
+      <div className="container grid grid-cols-1 md:grid-cols-3 gap-3 justify-center items-center">
+        <div>
+          <img src={logo} alt="Logo" className="h-20 w-20 rounded-full border-2 " />
+          <h1 className="font-bold">ChallengeHive</h1>
+        </div>
+        <div>
+          <h1>Copyright © 2025 ChallengeHive </h1>
+        </div>
+        <div  className="text-center mx-auto">
+          <ul>
+            <li className="text-2xl flex gap-2 font-bold items-center"> 
+              <FaFacebook /> FaceBook
+            </li>
+            <li className="text-2xl flex gap-2 font-bold items-center">
+              <FaLinkedin /> Linkedin
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
