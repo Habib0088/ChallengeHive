2069bec8a5fac3ada386a881b0faaf7045e7296d
Habib0088 <sarkerbatch2k19@gmail.com>
2025-12-16 15:59:23 +0600

    Winning percentage chart implemented

--- src/Component/Dashboard/Profile/Profile.jsx
+++ src/Component/Dashboard/Profile/Profile.jsx
@@ -1,43 +1,84 @@
-import React from 'react';
-import useAuth from '../../../hook/useAuth';
-import { useQuery } from '@tanstack/react-query';
-import useAxiosSecure from '../../../hook/useAxiosSecure/useAxiosSecure';
-import { Link } from 'react-router';
+import React from "react";
+import useAuth from "../../../hook/useAuth";
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../hook/useAxiosSecure/useAxiosSecure";
+import { Link } from "react-router";
+import { PieChart, Pie, Cell, Tooltip, Legend } from "recharts";
 
 const Profile = () => {
-    const {user}=useAuth()
-    const axiosSecure=useAxiosSecure()
-    const {data:userProfile}=useQuery({
-        queryKey:['users',user.email],
-        queryFn:async()=>{
-            const res=await axiosSecure.get(`/users/profile?email=${user.email}`)
-            return res.data
-        }
-    })
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const { data: userProfile } = useQuery({
+    queryKey: ["users", user.email],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/users/profile?email=${user.email}`);
+      return res.data;
+    },
+  });
 
-    const {data:count}=useQuery({
-        queryKey:['countWinning',user?.email],
-        queryFn:async()=>{
-            const res=await axiosSecure(`/user/stats?email=${user?.email}`)
-            return res.data
-        }
-    })
-    console.log(count);
-    
-    return (
-        <div className='p-4'>
-            <div className='max-w-8/12 mx-auto space-y-3 text-center rounded-md p-4 shadow-2xl bg-gray-200'>
-            <img  className='w-[150px] h-[150px] rounded-full mx-auto' referrerPolicy='no-referrer' src={userProfile?.photoURL} alt="Profile Pic" />
-                <h1 className='font-bold text-3xl'>{userProfile?.displayName}</h1>
-                <h1 className='font-bold text-xl'>{userProfile?.Bio || `Hi,I'm ${userProfile?.displayName}.I'm here as  ${userProfile?.role}`} </h1>
-                <div className='flex justify-around items-center '>
-                    <h2 className='font-bold text-xl'>{userProfile?.email}</h2>
-                    <h2 className='font-bold text-xl'>Role : {userProfile?.role}</h2>
-                </div>
-               <Link to='/dashboard/updateProfile'> <button className='btn bg-blue-600 text-white font-bold'>Update Profile</button></Link>
-            </div>
+  const { data: count } = useQuery({
+    queryKey: ["countWinning", user?.email],
+    queryFn: async () => {
+      const res = await axiosSecure(`/user/stats?email=${user?.email}`);
+      return res.data;
+    },
+  });
+  console.log(count);
+  const pieData = [
+    { name: "Participated", value: count?.totalParticipated || 0 },
+    { name: "Won", value: count?.totalWon || 0 },
+  ];
+
+  const COLORS = ["#2563eb", "#16a34a"];
+
+  return (
+    <div className="p-4">
+      <div className="max-w-8/12 mx-auto space-y-3 text-center rounded-md p-4 shadow-2xl bg-gray-200">
+        <img
+          className="w-[150px] h-[150px] rounded-full mx-auto"
+          referrerPolicy="no-referrer"
+          src={userProfile?.photoURL}
+          alt="Profile Pic"
+        />
+        <h1 className="font-bold text-3xl">{userProfile?.displayName}</h1>
+        <h1 className="font-bold text-xl">
+          {userProfile?.Bio ||
+            `Hi,I'm ${userProfile?.displayName}.I'm here as  ${userProfile?.role}`}{" "}
+        </h1>
+        <div className="flex justify-around items-center ">
+          <h2 className="font-bold text-xl">{userProfile?.email}</h2>
+          <h2 className="font-bold text-xl">Role : {userProfile?.role}</h2>
         </div>
-    );
+        <Link to="/dashboard/updateProfile">
+          {" "}
+          <button className="btn bg-blue-600 text-white font-bold">
+            Update Profile
+          </button>
+        </Link>
+      </div>
+      {/* Chart */}
+      <div>
+        <div className="flex justify-center mt-6">
+          <PieChart width={300} height={300}>
+            <Pie
+              data={pieData}
+              cx="50%"
+              cy="50%"
+              outerRadius={100}
+              dataKey="value"
+              label
+            >
+              {pieData.map((entry, index) => (
+                <Cell key={index} fill={COLORS[index]} />
+              ))}
+            </Pie>
+            <Tooltip />
+            <Legend />
+          </PieChart>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
